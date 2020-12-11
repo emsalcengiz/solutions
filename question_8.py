5d77b38782c7718fb3e1d3e8b2cc17ae56651bf0
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-12-11 21:22:23 +0300

    added reverse function

--- question_8.py
+++ question_8.py
@@ -1,19 +1,30 @@
-def fizzbuzz_word():
-    for i in range(50):
-	    if i % 3 == 0 and i % 5 == 0:
-	        print("fizzbuzz")
-	        continue
-	    elif i % 3 == 0:
-	        print("fizz")
-	        continue
-	    elif i % 5 == 0:
-	        print("buzz")
-	        continue
-	    print(i)
+def fizzbuzz_word(x=50):
+    f = open("fizzbuzz.txt", "w")
+    for i in range(1, x+1):
+        if i % 3 == 0 and i % 5 == 0:
+            word = "fizzbuzz"
+        elif i % 3 == 0:
+            word = "fizz"
+        elif i % 5 == 0:
+            word = "buzz"
+        else:
+            word= str(i)
+        f.write(word + '\n')
+    f.close()
+
+def fizzbuzz_word_reverse():
+    f_one = open("fizzbuzz.txt", "r")
+    f_two = open("fizzbuzz_reverse.txt", "w")
+    lines = f_one.readlines()
+    for line in lines:
+        f_two.write(line[::-1])
+
+    
 
 
 if __name__ == "__main__":
     fizzbuzz_word()
+    fizzbuzz_word_reverse()
 
 
 
