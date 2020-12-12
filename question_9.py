e12544f47e8aa624516ce8d061144d343e0d46d7
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-12-12 13:10:22 +0300

    added question_9

--- question_9.py
+++ question_9.py
@@ -9,16 +9,16 @@ nextyear = today.year+1
 for x in range(1, 7):
     tarih = date(nextyear, x, 1)
     if tarih.weekday() == 5:
-        tarih + timedelta(days=2)
+        tarih = tarih + timedelta(days=2)
     if tarih.weekday() == 6:
-        tarih + timedelta(days=1)
+         tarih = tarih + timedelta(days=1)
     os.mkdir(tarih.strftime("%d-%m-%Y"))
     lastDay = monthrange(tarih.year, tarih.month)[1]
     tarih = date(nextyear, x, lastDay)
     if tarih.weekday() == 5:
-        tarih - timedelta(days=2)
+        tarih = tarih - timedelta(days=1)
     if tarih.weekday() == 6:
-        tarih - timedelta(days=1)
+        tarih = tarih - timedelta(days=2)
     os.mkdir(tarih.strftime("%d-%m-%Y"))
 
 
